4a891a71dc85b8ba88ea68ef35b51ef585510d48
shanayabarretto <barretto.shanaya@gmail.com>
2022-10-29 11:16:42 -0400

    json parsing

---
+++ main.py
@@ -0,0 +1,12 @@
+import json
+import time
+import sys
+
+def main():
+    # first process the json file into a dict
+    file = open('test.json')
+    data = json.load(file)
+    
+
+if __name__ == "__main__":
+    sys.exit(main())
